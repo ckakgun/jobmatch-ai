b3410d67db6be857613df272ddcd7e87a93f047d
24-cka-ML <cerenkaya07@gmail.com>
2025-06-15 11:35:37 +0200

    Remove pymupdf from requirements file

---
+++ src/job_search.py
@@ -0,0 +1,37 @@
+import os
+import requests
+import json
+from dotenv import load_dotenv
+load_dotenv()
+
+def query_and_save_jobs(query, output_path="data/job_postings.json"):
+    '''
+    Search for jobs using the JSearch API and saves them to a json file
+    '''
+    headers = {
+        "X-RapidAPI-Key": os.getenv("JSEARCH_API_KEY"), # get the api key from the .env file
+        "X-RapidAPI-Host": "jsearch.p.rapidapi.com"
+    }
+    # Make GET request to JSearch API
+    url = 'https://jsearch.p.rapidapi.com/search'
+    params = {
+        'query': query,
+        'page': 1,
+        'num_pages': 1,
+        'date_posted': 'today',
+        'employment_type': 'full_time',
+        'sort': 'newest',
+        'limit': 10,
+    }
+    # Save response['data'] to job_postings.json
+    response = requests.get(url, headers = headers, params =params)
+    if response.status_code == 200:
+        data = response.json()
+        with open(output_path, 'w') as f:
+            json.dump(data, f, indent = 4)
+    return data
+
+if __name__ == "__main__":
+    query = "NLP Data Scientist"
+    data = query_and_save_jobs(query)
+    print(data)
---
+++ src/match_jobs.py
@@ -0,0 +1,7 @@
+def match_profile_to_jobs(profile, job_postings, top_n=3):
+    '''
+    Compare user profile against job postings.
+    Score/rank them based on keyword/skill overlap.
+    Return top N matches.
+    '''
+    pass  
